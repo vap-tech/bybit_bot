8fa5737d30daaf66f12efc86f27cfe5f8885b6e1
kot <bafomet2016vitt@gmail.com>
2024-03-21 22:55:43 +0300

    return to line grid strategy

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ ws.ticker_stream(
 def buy(qty=10.1):
 
     if qty < 10.1:
-        return
+        qty = 10.1
 
     count = 10
     while True:
@@ -71,7 +71,7 @@ def buy(qty=10.1):
 def sell(qty=0.000199):
 
     if qty < 0.000199:
-        return
+        qty = 0.000199
 
     count = 10
 
@@ -98,78 +98,29 @@ def sell(qty=0.000199):
     return response
 
 
-def get_balance(coin='BTC'):
-
-    count = 10
-
-    while True:
-
-        try:
-            response = session.get_wallet_balance(
-                accountType="UNIFIED",
-                coin=coin,
-            )
-            if response['retMsg'] == 'OK' or count <= 0:
-                break
-
-        except Exception as e:
-            print(e)
-            sleep(1)
-            count -= 1
-            if count <= 0:
-                return '0'
-
-    return response['result']['list'][0]['coin'][0]['equity']
-
-
-def get_qty_to_trade() -> dict:
-    btc = float(get_balance('BTC'))
-    usdc = float(get_balance('USDC'))
-    return {'BTC': btc, 'USDC': usdc}
-
-
-def set_qty_to_trade(qty_to_trade: dict) -> None:
-
-    btc = qty_to_trade['BTC'] / 5
-    _btc = float('{:.6f}'.format(btc))
-    if _btc < 0.000199:
-        _btc = 0.000199
-    r.set('qty_btc', _btc)
-
-    usdc = qty_to_trade['USDC'] / 5
-    _usdc = float('{:.6f}'.format(usdc))
-    if _usdc < 10.1:
-        _usdc = 10.1
-    r.set('qty_usdc', _usdc)
-
-
 set_old_price(r.get('price'))
 r.set(name='diff', value=300)
-set_qty_to_trade(get_qty_to_trade())
+
 
 while True:
 
     difference = float(r.get('diff'))
     old_price = float(r.get('old_price'))
     new_price = float(r.get('price'))
-    qty_btc = float(r.get('qty_btc'))
-    qty_usdc = float(r.get('qty_usdc'))
 
     if new_price - old_price > difference:
 
         print(datetime.now().strftime("%H:%M:%S"), 'S', end=' ')
-        print(sell(qty_btc))
-        print(f'qty_btc: {qty_btc}, price: {new_price}')
+        print(sell())
+        print(f'price: {new_price}')
         set_old_price(new_price)
-        set_qty_to_trade(get_qty_to_trade())
         continue
 
     if old_price - new_price > difference:
 
         print(datetime.now().strftime("%H:%M:%S"), 'B', end=' ')
-        print(buy(qty_usdc))
-        print(f'qty_usdc: {qty_usdc}, price: {new_price}')
+        print(buy())
+        print(f'price: {new_price}')
         set_old_price(new_price)
-        set_qty_to_trade(get_qty_to_trade())
 
     sleep(1)
